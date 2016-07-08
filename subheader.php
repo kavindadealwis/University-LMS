463414d9775893edf487d61e3fcb0681d1708b67
kavindadealwis <kavindadealwis@gmail.com>
2016-07-08 18:56:17 +0530

    Add files via upload

---
+++ subheader.php
@@ -0,0 +1,23 @@
+<link rel="stylesheet" href=".css">
+<link href="Bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<body>
+
+<div class="container">
+    
+
+    <div class="col-md-12">
+    <div class="header clearfix">
+        <nav>
+            <ul class="nav nav-pills pull-right">
+                <li><marquee>Waiting For O/l or A/L Results......Choose Your Future Carrier Degree Programme......New Degree Programms are coming soon!!!......</marquee></li>
+                <li><a href="#"><span class="glyphicon glyphicon-envelope"></span> Messages</a></li>
+                <li><a href="profile.php"><span class="glyphicon glyphicon-user"></span> Profile</a></li>
+            </ul>
+        </nav>
+    </div>
+
+    </div>
+
+
+</div>
